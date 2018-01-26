00852605acf901b3773e6ffc32b138cd363a52e0
Sebastian Sandqvist <seb@sparque.me>
2018-01-25 19:14:04 -0800

    support old node versions

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 // - @param date {String|Number|Date}
 // - @return {Number} years since date
 module.exports = function(date) {
-  const type = Object.prototype.toString.call(date);
-  const now = new Date();
+  var type = Object.prototype.toString.call(date);
+  var now = new Date();
 
   // convert dateStrings and timestamps to a Date
   if (type === '[object String]' || type === '[object Number]') {
     date = new Date(date);
   }
 
-  let yearDiff = now.getFullYear() - date.getFullYear();
-  const monthDiff = now.getMonth() - date.getMonth();
-  const pastDate = now.getDate() < date.getDate();
+  var yearDiff = now.getFullYear() - date.getFullYear();
+  var monthDiff = now.getMonth() - date.getMonth();
+  var pastDate = now.getDate() < date.getDate();
 
   // compare months. if same month, compare days
   if (monthDiff < 0 || (monthDiff === 0 && pastDate)) {
